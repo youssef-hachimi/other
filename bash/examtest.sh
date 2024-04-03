7ae09766a4263f488fcbe07aefd9d31d9132c811
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-04-03 15:05:15 +0000

    Update examtest.sh

--- bash/examtest.sh
+++ bash/examtest.sh
@@ -1,23 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 #Initialisation de la somme des notes et du compteur de notes
-somme notes=0
-ompteur notes=0
-While [ $compteur notes -lt 8 ]; do
-read -P "Entrez la note pour le module $((comp teur notes X D
-..
-if [ Snote -ge 0 ]; then
-  i£ [ Snote -gt 12 ]; then
-    echo "Module $ ( (compteur notes + 1)) : Validé"
-      else
-      echo "Module $ ( (compteur notes + 1)) : Non validé"
-    note
+somme_notes=0
+ompteur_notes=0
+While [ $compteur_notes -lt 8 ]; do
+read -P "Entrez la note pour le module $((compteur_notes + 1 )) : "note
+if [ $note -ge 0 ]; then
+  if [ Snote -gt 12 ]; then
+    echo "Module $((compteur notes + 1)) : Validé"
+  else
+      echo "Module $((compteur notes + 1)) : Non validé"
     fi
-somme notes=$ ( (somme notes t note))
-((compteur notest+))
+  somme_notes=$ ((somme_notes + note))
+((compteur_notes++))
 else
-echo "Veuillez saisir une note positive."
+    echo "Veuillez saisir une note positive."
 fi
 done
-thoyenne=$ ( (somme notes / 8))
-# moyenne=$ ( (somme notes / compteur notes))
-echo "La moyenne des notes saisies est : $movenne"
+moyenne=$ ((somme notes / 8))
+# moyenne=$ ((somme notes / compteur notes))
+echo "La moyenne des notes saisies est : $moyenne"
